65397e9d6f805e1f70c2a6b0d737b43fc64976d9
ayan471 <pakhiraayan@gmail.com>
2024-09-25 15:51:06 +0530

    addded google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Head from "next/head"; // Import Head component
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "12otwelve",
-  description: "Ecommece media agency provider !",
+  description: "Ecommerce media agency provider!",
 };
 
 export default function RootLayout({
@@ -18,6 +19,22 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=AW-16709628315"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-16709628315');
+              `,
+            }}
+          />
+        </Head>
         <body className={inter.className}>
           <div>{children}</div>
         </body>
